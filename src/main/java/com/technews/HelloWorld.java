df65b2c004a0bc637855283999a15df7645b3c2c
rs0579 <raheemsenegal@outlook.com>
2025-05-21 15:31:51 -0500

    Added files to com.technews repo. learned about variables and Java's  - System.out.println()

---
+++ src/main/java/com/technews/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.technews;
+
+public class HelloWorld { public static void main(String[] args){
+    System.out.println("Hello, World");
+}
+}
---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariableTypes { public static void  main(String[] args){
+    int amount = 54;
+    double price = 5.99;
+    String carrot = "You ought to know better, since it is a string, you must use quotes.";
+
+    double sum = amount + price;
+    String concat = amount + carrot;
+
+    System.out.println(sum);
+    System.out.println(concat);
+}
+}
